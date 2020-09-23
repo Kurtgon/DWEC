a1780e80f8ba55cbda6d0d35374ebec7fea36ce3
Kurtgon <gongmateos@gmail.com>
2020-09-23 20:36:24 +0200

    Añadiendo el ejercicio 3 y el tablero

--- Ejercicios_JavaScript_avanzados/Ejercicio1/scripts.js
+++ Ejercicios_JavaScript_avanzados/Ejercicio1/scripts.js
@@ -1,3 +1,5 @@
+
+//Ejercicio 1
 let capa = document.getElementById("box");
 
 capa.addEventListener("mouseenter",(e)=>{
@@ -21,6 +23,7 @@ capa.addEventListener("mouseout",(e)=>{
     console.log("Has soltado el botón izquierdo dentro de la caja");
 })
 
+//Ejercicio 2
 let tecla = document.getElementById("cajaTexto");
 
 tecla.addEventListener("keydown", (e)=>{
@@ -37,3 +40,39 @@ tecla.addEventListener("keypress",(e)=>{
     let letra = e.key;
     console.log(`La tecla pulsada es ${letra}`);
 })
+
+//Ejercicio 3
+let boton = document.getElementById("botonEnvio");
+
+boton.addEventListener("click",(e)=>{
+    e.preventDefault();
+    capturaTecla();
+    //Para sacar información y ver las propiedades
+    console.log(e.target);
+
+})
+//Creamos la función para capturar el input del formulario
+ const capturaTecla = ()=>{
+     
+    let textoCapturado = document.getElementById("nombre");
+    //Creamos el evento de la captura de teclas
+    textoCapturado.addEventListener("keyup",(e)=>{
+        console.log(e.key);
+    })
+
+ }
+
+ //Tablero
+ let galeria = document.getElementById("gallery");
+ 
+ galeria.addEventListener("click",(e)=>{
+     e.preventDefault();
+     if(e.target.className.match("gallery_item")){
+     e.target.classList.replace('gallery_item','gallery_click');
+    }else{
+        e.target.classList.replace('gallery_click','gallery_item');
+    }
+     
+ })
+
+
